cb7bbec087c08f394b899836834a2bb6ce2d0996
NguyenT01 <truongdinhkien321@gmail.com>
2022-01-04 19:00:07 +0700

    Add database

--- RiceAgentManageSystem/RiceAgentManageSystem/ManageForm.Designer.cs
+++ RiceAgentManageSystem/RiceAgentManageSystem/ManageForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ManageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1964, 949);
+            this.Name = "ManageForm";
             this.Text = "ManageForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
