5f968be412b2c549135ad9018b127cba358352d5
NguyenT01 <truongdinhkien321@gmail.com>
2022-01-04 21:16:04 +0700

    Add website

---
+++ RiceAgentWebsite/RiceAgentWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RiceAgentWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
